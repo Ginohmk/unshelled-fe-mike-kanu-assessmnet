c0b6679f2e3915664f050f14e6ffe48ecda35a95
ginohmk <michotall95@gmail.com>
2024-06-19 19:14:40 +0100

    feat: add router for routes to pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import ItemList from './pages/ItemList/ItemList';
+import ItemDetail from './pages/ItemDetail/ItemDetail';
+
 function App() {
-  return <h1>Home</h1>;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/items" element={<ItemList />} />
+      <Route path="/items/:id" element={<ItemDetail />} />
+    </Routes>
+  );
 }
 
 export default App;
